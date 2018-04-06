7b7bc1c09ce17b478ec164d93757a2282ce16fe6
vladsaveliev <vladislav.sav@gmail.com>
2018-04-06 16:09:18 +1000

    Separated DiploidNeverResponder into separate md file

--- insertions/assemble/filter_bam_highqual.py
+++ insertions/assemble/filter_bam_highqual.py
@@ -11,7 +11,12 @@ bamf_hqual_out = join(dirname(bam), 'lng_hqual', basename(bam))
 bamf = pysam.AlignmentFile(bam, "rb")
 bamf_hqual_f = pysam.AlignmentFile(bamf_hqual_out, "wb", template=bamf)
 
+total_reads = 0
+total_good = 0
+
 for read in bamf:
+    total_reads += 1
+
     lng = read.query_length >= 125
     if not read.query_qualities:
         continue
@@ -20,9 +25,10 @@ for read in bamf:
 
     if read.is_paired and lng and hqual:
         bamf_hqual_f.write(read)
+        total_good += 1
 
 bamf.close()
 bamf_hqual_f.close()
 
-print('Done. Stats:')
+print(f'Done. Written {total_good} out of {total_reads}')
 
