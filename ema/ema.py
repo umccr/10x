e49a2050f9fe52802991f723f8db023d3a456d6a
vladsaveliev <vladislav.sav@gmail.com>
2018-06-28 23:41:39 +1000

    Add EMA Snakemake pipeline

--- ema/ema.py
+++ ema/ema.py
@@ -7,13 +7,14 @@ import shutil
 import click
 import subprocess
 
+import yaml
 from ngs_utils.call_process import run_simple
 from ngs_utils.file_utils import verify_file, safe_mkdir, verify_dir
 from ngs_utils import logger
 from ngs_utils.logger import critical
-from ngs_utils.utils import set_locale;
-from python_utils.hpc import get_ref_file
-set_locale()
+from ngs_utils.utils import set_locale; set_locale()
+from python_utils import hpc
+from ngs_utils import snakemake_utils
 
 
 def package_path():
@@ -37,7 +38,7 @@ ema.py /data/cephfs/punim0010/data/FASTQ/180312_A00130_0041_AHCLLMDMXX/Chromium_
 
 
 @click.command()
-@click.argument('r1_fastq_paths', nargs=4, type=click.Path(exists=True))
+@click.argument('r1_fastq_paths', nargs=-1, type=click.Path(exists=True))
 @click.option('-o', 'output_dir', type=click.Path(), help='Output directory (default is "ema_<sample_name>")')
 @click.option('-j', '--jobs', 'jobs', default=1, help='Maximum number of cores to use at single time (works both for local '
               'and cluster runs)')
@@ -45,10 +46,11 @@ ema.py /data/cephfs/punim0010/data/FASTQ/180312_A00130_0041_AHCLLMDMXX/Chromium_
 @click.option('-c', '--cluster-auto', 'cluster', is_flag=True, help='Submit jobs to cluster')
 @click.option('-g', '--genome', help='Genome build (GRCh37 or hg38)', type=click.Choice(['GRCh37', 'hg38']), default='GRCh37')
 @click.option('--unlock', is_flag=True, help='Propagaded to snakemake')
+@click.option('--bc-whitelist', help='Whitelist of 10x barcodes')
 @click.option('--bcbio-genomes', help='Path to bcbio-nextgen reference data (e.g. /bcbio/genomes; '
               'Hsapiens/{genome}/seq/{genome}.fa(.fai) and Hsapiens/{genome}/validation/giab-NA12878/truth_regions.bed are used)')
 def main(r1_fastq_paths, output_dir=None, jobs=None, sample_name=None,
-         cluster=False, genome=None, unlock=False, bcbio_genomes=None):
+         cluster=False, genome=None, unlock=False, bc_whitelist=None, bcbio_genomes=None):
     """
 EMA wrapper.\n
 r1_fastq_paths: paths to R1 fastq files for each sample\n
@@ -61,18 +63,19 @@ r1_fastq_paths: paths to R1 fastq files for each sample\n
     log_dir = safe_mkdir(join(output_dir, 'log'))
     logger.init(log_fpath_=join(log_dir, 'ema.log'), save_previous=True)
 
+    #################################
+    #### Making snakemake config ####
+    #################################
+
     conf = dict()
+    conf['r1_fastq_paths'] = [verify_file(fp, is_critical=True) for fp in r1_fastq_paths]
 
-    #######################
-    #### Setting paths ####
-    #######################
-
-    conf['r1_fastqs_paths'] = r1_fastq_paths
-
+    ###########################
+    #### Setting ref paths ####
     ref_fa = None
     bwa_fa = None
+    # Looking for reference fasta in bcbio_genomes folder?
     if bcbio_genomes:
-        # Looking for reference fasta
         for subdir in [join(bcbio_genomes, f'Hsapiens/{genome}'),
                        join(bcbio_genomes, f'{genome}'),
                        join(bcbio_genomes)]:
@@ -87,27 +90,34 @@ r1_fastq_paths: paths to R1 fastq files for each sample\n
         if not bwa_fa:
             critical(f'Not found BWA indices bwa/{genome}.fa* in bcbio genomes directory {bcbio_genomes}')
 
-    #################################################################
-    #### Reference files not provided, but we are on known host? ####
-    #################################################################
+    # Reference files not provided, but we are on known host?
     if not ref_fa:
-        conf['ref_fa'] = get_ref_file(genome)
+        ref_fa = hpc.get_ref_file(genome)
     if not bwa_fa:
-        conf['bwa_fa'] = get_ref_file(genome, ['bwa'], must_exist=False)
+        bwa_fa = hpc.get_ref_file(genome, ['bwa'], must_exist=False)
+    if not bc_whitelist:
+        bc_whitelist = hpc.get_loc().barcodes_10x
 
-    ###########################################################
-    #### Done looking for refernece files, now some checks ####
-    ###########################################################
+    # Done looking for refernece files, now some checks
     ref_fa = verify_file(ref_fa, is_critical=True, description='Reference fasta')
     verify_file(ref_fa + '.fai', is_critical=True, description='Reference fasta fai index')
     verify_file(bwa_fa + '.bwt', is_critical=True, description='BWA index')
+    verify_file(bc_whitelist, is_critical=True)
+
+    # Filling up conf
+    conf['ref_fa'] = ref_fa
+    conf['bwa_fa'] = bwa_fa
+    conf['bc_whitelist'] = bc_whitelist
 
     #####################################
     #### Setting non-path parameters ####
-    #####################################
-
     if sample_name:
         conf['sample'] = sample_name
+
+    # Saving config to file
+    conf_path = join(output_dir, 'conf.yml')
+    with open(conf_path, 'w') as out:
+        yaml.dump(conf, out)
 
     #########################
     #### Setting cluster ####
@@ -115,19 +125,15 @@ r1_fastq_paths: paths to R1 fastq files for each sample\n
 
     cluster_param = ''
     if cluster:
-        from python_utils.hpc import get_loc
-        loc = get_loc()
-        if not loc.submit_job_cmd:
-            logger.critical(f'Automatic cluster submission is not supported for the machine "{loc.name}"')
-        cluster_wrapper = join(package_path(), 'snakemake_submit.py')
-        cluster_cmd = f'python {cluster_wrapper}'
-        cluster_param = f' --cluster "{cluster_cmd}"'
+        log_dir = safe_mkdir(join(log_dir, 'cluster'))
+        cluster_param = snakemake_utils.make_cluster_cmdl(log_dir)
 
     ###############################
     #### Building command line ####
     ###############################
 
     snakefile = join(package_path(), 'Snakefile')
+    # import pdb; pdb.set_trace()
     cmd = (
         f'snakemake '
         f'--snakefile {snakefile} '
@@ -136,7 +142,7 @@ r1_fastq_paths: paths to R1 fastq files for each sample\n
         f'-j {jobs} '
         f'--rerun-incomplete ' 
         f'{cluster_param} '
-        f'--config {" ".join(k + "=" + v for k, v in conf.items())} '
+        f'--configfile {conf_path} '
     )
 
     if unlock:
